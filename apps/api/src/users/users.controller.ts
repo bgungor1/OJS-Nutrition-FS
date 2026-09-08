62f4ec116d2f59595dcf63e86a23f6b79bf36293
Berkay Güngör <berkayggngr@hotmail.com>
2026-09-08 12:04:07 +0300

    test(users): add user profile e2e integration test suite

--- apps/api/src/users/users.controller.ts
+++ apps/api/src/users/users.controller.ts
@@ -25,7 +25,7 @@ import { UsersService } from './users.service';
 @ApiBearerAuth()
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) { }
+  constructor(private readonly usersService: UsersService) {}
 
   @Get('my-account')
   @HttpCode(HttpStatus.OK)
